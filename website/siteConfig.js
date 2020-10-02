9a412cf5f41e35433c16806f4c6b9ef92beee7a4
acp15 <anjalipachpute15@gmail.com>
2020-10-02 12:33:40 -0700

    Update to tech-workfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Anjali Pachpute', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://anjali15.github.io', // Your website URL
+    url: 'https://acp15.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
